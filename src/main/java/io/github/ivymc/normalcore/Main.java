44f1da5b1373d3e70095afd87c2c1593ee7d1556
JustFoxx <justfoxxo@outlook.com>
2022-11-07 17:44:46 +0100

    Now it works on integrated servers.

--- src/main/java/io/github/ivymc/normalcore/Main.java
+++ src/main/java/io/github/ivymc/normalcore/Main.java
@@ -5,10 +5,11 @@ import io.github.ivymc.normalcore.config.Registry;
 import io.github.ivymc.normalcore.event.Event;
 import io.github.ivymc.normalcore.helper.PlayerHelper;
 import net.fabricmc.api.DedicatedServerModInitializer;
+import net.fabricmc.api.ModInitializer;
 
-public class Main implements DedicatedServerModInitializer {
+public class Main implements ModInitializer {
     @Override
-    public void onInitializeServer() {
+    public void onInitialize() {
         PlayerHelper.register();
         Event.init();
     }
